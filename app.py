9e4c106ef5944090c36aa1c73a71e251c37b68bc
Aditya Kotwal <kotwala@kotwala-a01.vmware.com>
2019-03-21 13:44:08 -0700

    Update Mars

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
    target = os.environ.get('TARGET', 'World')
-   return 'Hello {} Welcome to Mars!\n'.format(target)
+   return 'Hello {} Welcome to Mars - your new home!\n'.format(target)
 
 if __name__ == "__main__":
    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
